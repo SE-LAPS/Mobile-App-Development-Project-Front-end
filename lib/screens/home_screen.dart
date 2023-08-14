694f6bc67bca0d08fa06a6b0d73ef498a7983cf2
Lahiru Senavirathna <87580847+SE-LAPS@users.noreply.github.com>
2023-08-15 00:31:00 +0530

    Create the Mobile App First Interfaces
    Login , Signup , Verification Mail , Registration , Successfully Login Home

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:Dine Delish App/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:food_app/screens/welcome_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final String? userName = FirebaseAuth.instance.currentUser?.displayName;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(" Homepage"),
+      ),
+      body: Column(
+        children: [
+          Text("Hello $userName"),
+          ElevatedButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+                if (!mounted) return;
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return const WelcomeScreen();
+                }));
+              },
+              child: const Text("Sign Out"))
+        ],
+      ),
+    );
+  }
+}
