fb72db64c8c893cf6335d5b6912003888f6641a7
Lahiru Senavirathna <87580847+SE-LAPS@users.noreply.github.com>
2023-10-03 04:20:54 +0530

    Add the Carousel Slider
    Add the Carousel Slider dart file

--- main.dart
+++ main.dart
@@ -1,21 +1,23 @@
-import 'package:flutter/material.dart';
-import 'package:food_onboarding_lottie/views/onboarding_screen.dart';
-
 void main() {
-  runApp(const MyApp());
+runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: const OnboardingScreen(),
-    );
-  }
+// This widget is the root of your application.
+@override
+Widget build(BuildContext context) {
+	return MaterialApp(
+	debugShowCheckedModeBanner: false,
+		
+	// Title of App
+	title: 'GFG slider',
+	theme: ThemeData(
+		primarySwatch: Colors.green,
+	),
+	darkTheme: ThemeData.dark(),
+		
+	//First Screen of Slider App
+	home: HomePage(),
+	);
+}
 }
