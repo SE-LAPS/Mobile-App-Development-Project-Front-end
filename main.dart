81c97c61d81c4014fbfbbf2443d27ea89f824e21
Lahiru Senavirathna <87580847+SE-LAPS@users.noreply.github.com>
2023-10-05 04:59:46 +0530

    Flutter Google Map With Live Location Tracking
    Flutter Google Map With Live Location Tracking Dart files

--- main.dart
+++ main.dart
@@ -1,23 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_mao/order_traking_page.dart';
+
 void main() {
-runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-// This widget is the root of your application.
-@override
-Widget build(BuildContext context) {
-	return MaterialApp(
-	debugShowCheckedModeBanner: false,
-		
-	// Title of App
-	title: 'GFG slider',
-	theme: ThemeData(
-		primarySwatch: Colors.green,
-	),
-	darkTheme: ThemeData.dark(),
-		
-	//First Screen of Slider App
-	home: HomePage(),
-	);
-}
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0,
+        ),
+      ),
+      home: const OrderTrackingPage(),
+    );
+  }
 }
