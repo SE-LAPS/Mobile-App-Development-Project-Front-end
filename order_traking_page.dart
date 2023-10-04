9ec5aacd3d41c4511d61b8c1ed022da42a0797ae
Lahiru Senavirathna <87580847+SE-LAPS@users.noreply.github.com>
2023-10-05 04:57:04 +0530

    flutter google map with location tracking
    flutter google map with location tracking Dart file

---
+++ order_traking_page.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class OrderTrackingPage extends StatefulWidget {
+  const OrderTrackingPage({Key? key}) : super(key: key);
+
+  @override
+  State<OrderTrackingPage> createState() => OrderTrackingPageState();
+}
+
+class OrderTrackingPageState extends State<OrderTrackingPage> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  static const LatLng sourceLocation = LatLng(37.33500926, -122.03272188);
+  static const LatLng destination = LatLng(37.33429383, -122.06600055);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Track order",
+          style: TextStyle(color: Colors.black, fontSize: 16),
+        ),
+      ),
+      body: const Center(
+        child: Text("The Flutter Way!"),
+      ),
+    );
+  }
+}
